adcd62df9ce4d51fd1cd8e22c9fd9fd7aa031ab8
godwinricafort <163822404+godwinricafort@users.noreply.github.com>
2024-08-30 15:13:06 +0800

    Add files via upload

---
+++ Escseq.java
@@ -0,0 +1,14 @@
+class Esceq {
+    public static void main(String[] args) {
+        
+        int n = 7;
+        for (int i = 1; i < 6; i++) {
+            if (i % 2 == 1) {
+                System.out.println(n);
+            } else {
+                System.out.println("\t" + n);
+            }
+            n += 7;
+        }
+    }
+}
