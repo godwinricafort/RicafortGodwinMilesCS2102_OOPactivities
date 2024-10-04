97279e47bea4d9048bfa2ed11ca83e91f82e826d
godwinricafort <163822404+godwinricafort@users.noreply.github.com>
2024-10-04 16:09:50 +0800

    Update GettingInputs.java

--- Laboratory Activities/Laboratory Activity 1 - 06-09-2024/GettingInputs.java
+++ Laboratory Activities/Laboratory Activity 1 - 06-09-2024/GettingInputs.java
@@ -1,28 +1,34 @@
 import java.util.Scanner;
 
-class GettingGreater {
+class GettingInputs {
     public static void main(String[] args) {
-        Scanner Values = new Scanner(System.in);
-        System.out.print("Getting the Greater Value\n");
-        
-        System.out.print("Enter first character: ");
-        char First = Values.next().charAt(0);
-        int AsciiFirst = (int) First;
-        
-        System.out.print("Enter second character: ");
-        char Second = Values.next().charAt(0);
-        int AsciiSecond = (int) Second;
-        
-        int GreaterAscii = Math.max(AsciiFirst, AsciiSecond);
-        char GreaterChar = (char) GreaterAscii;
-        
-        System.out.println("-----------------------------------------------");
-        System.out.println("The character with the greater value is: " + GreaterChar);
-        System.out.println("-----------------------------------------------");
-        
-        System.out.println("Showing the ASCII Codes");
-        System.out.println(First + " : " + AsciiFirst);
-        System.out.println(Second + " : " + AsciiSecond);
-        Values.close();
+        Scanner Inputs = new Scanner(System.in);
+
+        System.out.print("Enter the year: ");
+        int Year = Inputs.nextInt();
+        Inputs.nextLine();
+
+        System.out.print("Enter the genre: ");
+        String Genre = Inputs.nextLine();
+
+        System.out.print("Enter the album: ");
+        String Album = Inputs.nextLine();
+
+        System.out.print("Enter the song title: ");
+        String Song = Inputs.nextLine();
+
+        System.out.print("Enter the artist: ");
+        String Artist = Inputs.nextLine();
+
+        System.out.println("-----------------------------");
+        System.out.println("SONG DETAILS");
+        System.out.println("-----------------------------");
+
+        System.out.println("Year released: " + Year);
+        System.out.println("Genre: " + Genre);
+        System.out.println("Album: " + Album);
+        System.out.println("Title: \"" + Song + "\"");
+        System.out.println("Artist: " + Artist);
+        Inputs.close();
     }
 }
